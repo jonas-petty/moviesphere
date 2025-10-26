b23033cb7f85be20387a4994ef22546929be8f3c
Jonas Felix <jonaspetty@hotmail.com>
2025-10-26 21:34:35 +0000

    feat: add movies search  and searchById endpoints

---
+++ moviesphere-backend/src/routes/movies.routes.ts
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import z from "zod";
+import { searchMovies, getMovieById } from "../services/tmdb.service.js";
+
+const router = Router();
+
+router.get("/search", async (req, res, next) => {
+    try {
+        const schema = z.object({
+            query: z.string().min(1),
+            page: z.string().optional(),
+        });
+
+        const { query, page } = schema.parse(req.query);
+        const data = await searchMovies(query, page ? Number(page) : 1);
+        res.json(data);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get("/:id", async (req, res, next) => {
+    try {
+        const { id } = z.object({ id: z.string() }).parse(req.params);
+        const data = await getMovieById(id);
+        res.json();
+    } catch (error) {
+        next(error);
+    }
+});
+
+export default router;
