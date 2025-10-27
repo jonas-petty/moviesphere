32ac1b525f7763b3e913d9f811f8bca24821a689
Jonas Felix <jonaspetty@hotmail.com>
2025-10-27 20:11:26 +0000

    feat: empty favorites page content

--- moviesphere-frontend/src/pages/Favorites.tsx
+++ moviesphere-frontend/src/pages/Favorites.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { api } from "../services/api";
-import { Link } from "react-router-dom";
+import Header from "../components/Header";
+import styles from "../styles/pageLayout.module.css";
+import Button from "../components/Button";
 
 function Favorites() {
     const [list, setList] = useState<any[]>([]);
@@ -29,33 +31,38 @@ function Favorites() {
     }
 
     return (
-        <div id="container">
-            <h1>Favoritos</h1>
-            <Link to="/">Buscar</Link>
-            <button disabled={!list.length} onClick={share}>
-                Gerar Link
-            </button>
-            {shareUrl && (
-                <p>
-                    Compartilhe: <a href={shareUrl}>{shareUrl}</a>
-                </p>
-            )}
-            <div id="movies-container">
-                {list.map((movie: any) => (
-                    <div key={movie.id} className="movie-card">
-                        <img
-                            src={`https://image.tmdb.org/t/p/w200${movie.posterPath}`}
-                            alt="Movie poster"
-                        />
-                        <h3>{movie.title}</h3>
-                        <p>TMDB: {movie.rating?.toFixed(1)}</p>
-                        <button onClick={() => remove(movie.movieId)}>
-                            Romover
-                        </button>
-                    </div>
-                ))}
+        <>
+            <Header />
+            <div className={styles.container}>
+                <h2>Favoritos</h2>
+                {!list.length && (
+                    <p>Adicione filmes aos favoritos para velos aqui!</p>
+                )}
+                <Button disabled={!list.length} onClick={share}>
+                    Gerar Link
+                </Button>
+                {shareUrl && (
+                    <p>
+                        Compartilhe: <a href={shareUrl}>{shareUrl}</a>
+                    </p>
+                )}
+                <div id="movies-container">
+                    {list.map((movie: any) => (
+                        <div key={movie.id} className="movie-card">
+                            <img
+                                src={`https://image.tmdb.org/t/p/w200${movie.posterPath}`}
+                                alt="Movie poster"
+                            />
+                            <h3>{movie.title}</h3>
+                            <p>TMDB: {movie.rating?.toFixed(1)}</p>
+                            <button onClick={() => remove(movie.movieId)}>
+                                Romover
+                            </button>
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
