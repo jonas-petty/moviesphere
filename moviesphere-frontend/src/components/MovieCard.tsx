b7b33dda0f2328ecae9c92f7b786dd785a88a51e
Jonas Felix <jonaspetty@hotmail.com>
2025-10-27 19:50:06 +0000

    featL: add the movie card comoponent

---
+++ moviesphere-frontend/src/components/MovieCard.tsx
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import styles from "../styles/pageLayout.module.css";
+
+const StyledMovieCard = styled.div`
+    max-width: 25rem;
+    background-color: var(--secondary-background-color);
+    padding: 1rem;
+    border-radius: 0.5rem;
+    transition: transform 0.2s;
+
+    &:hover {
+        transform: scale(1.02);
+    }
+`;
+
+const Poster = styled.img`
+    width: 100%;
+    border-radius: 4px;
+    margin-bottom: 0.5rem;
+`;
+
+const Info = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+function MovieCard({ movie, action }: any) {
+    return (
+        <StyledMovieCard key={movie.id || movie.movieId}>
+            <Poster
+                src={`https://image.tmdb.org/t/p/w300${
+                    movie.poster_path || movie.posterPath
+                }`}
+            />
+            <Info>
+                <h2>{movie.title}</h2>
+                <p>TMDB: {movie.rating || movie.vote_average}</p>
+                {action}
+                <Link
+                    className={styles.link}
+                    to={`/movie/${movie.id || movie.movieId}`}
+                >
+                    Detalhes
+                </Link>
+            </Info>
+        </StyledMovieCard>
+    );
+}
+
+export default MovieCard;
